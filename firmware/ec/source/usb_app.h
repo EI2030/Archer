48f407731b430edf29a82469e8ba7431a865ad41
Wenting Zhang <zephray@outlook.com>
2021-05-24 22:55:23 -0400

    fw: initial ec firmware

---
+++ firmware/ec/source/usb_app.h
@@ -0,0 +1,14 @@
+/*
+ * usb_app.h
+ *
+ *  Created on: May 18, 2021
+ *      Author: wenting
+ */
+
+#ifndef USB_APP_H_
+#define USB_APP_H_
+
+void debug_usb_init(void);
+void debug_usb_task(void);
+
+#endif /* USB_APP_H_ */
